594506709ecce65687334d347e09ce4f6b8a6848
g1925417 <g1925417@yahoo.co.jp>
2020-03-21 10:00:45 +0900

    seed_fu不具合

--- db/fixtures/02_post_images.rb
+++ db/fixtures/02_post_images.rb
@@ -3,6 +3,7 @@ Post.all.each do |p|
   # 対応する画像があるか確認
   unless p.image.attached?
     # 画像をattachメソッドで登録(登録するデータはpathを指定, filenameでファイルを選択)
-    p.image.attach(io: File.open("db/fixtures/images/post/#{Random.rand(1..9)}.jpg"), filename: "#{Random.rand(1..9)}.jpg")
+    filename = Dir.open("db/fixtures/images/post",&:to_a).sample
+    p.image.attach(io: File.open("db/fixtures/images/post/#{filename}"), filename: "#{filename}")
   end
 end
--- db/migrate/20200320002536_remove_user_id_from_posts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdFromPosts < ActiveRecord::Migration[6.0]
-  def change
-    remove_reference :posts, :user, null: false, foreign_key: true
-  end
-end
